032e1924c73eebb86d07f7a88e5e7ddae0a7d5c3
Muhammad Ossama Khan/Retail Sales Central B Region/Consumer Business/Multan <muhammad.ossama1@ptclgroup.com>
2026-07-23 12:11:49 +0500

    fixed storage

--- core/settings.py
+++ core/settings.py
@@ -133,12 +133,6 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-STORAGES = {
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
-
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
